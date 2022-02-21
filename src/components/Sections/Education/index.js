89c8386a4a08b833220b945ba5d5e2ab1a26f101
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-02-21 18:16:26 -0300

    add education section

---
+++ src/components/Sections/Education/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Section from '../index';
+
+import { 
+  Text,
+  List,
+  ItemList
+} from '../styled';
+
+export default function Education() {
+  const data = {
+    'Universidade Federal de Campina Grande [UFCG]': ['2018 - Atualmente', 'Bacharelado em Ciência da computação.'],
+    'Instituto Federal de Pernambuco [IFPE]': ['2014 - 2016', 'Grau técnico em informática para internet integrado com o ensino médio.']
+  };
+
+  return (
+    <Section title="Educação">
+      <List>
+      {Object.entries(data).map(([key, items]) => (
+          <ItemList key={key}>
+            <Text className="bold">{key}</Text>
+            {items.map((item) => (
+              <Text>{item}</Text>
+            ))}
+          </ItemList>
+      ))}
+      </List>
+    </Section>
+  );
+}
