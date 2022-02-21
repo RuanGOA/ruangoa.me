566ebfe80af3274b14d21112403574005686dfc3
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-02-21 18:15:56 -0300

    add carrer section

---
+++ src/components/Sections/Carrer/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Section from '../index';
+
+import { 
+  Text,
+  List,
+  ItemList
+} from '../styled';
+
+export default function Carrer() {
+  const data = {
+    'ePol - Software Practices Laboratory [SPLab]': ['Desenvolvedor e Pesquisador', 'Março/2020 - Atualmente', 'Realizei pesquisas de modelos de Machine Learning utilizando Python; Desenvolvi APIs REST em Flask (Python); Desenvolvi um protótipo frontend para integração com APIs desenvolvidas no projeto, utilizanndo React (JavaScript); Utilizei ferramentas de DevOps como Docker, OpenShift e Jenkins, realizando implantações em ambientes de produção e de homologação.']
+  };
+
+  return (
+    <Section title="Carreira">
+      <List>
+      {Object.entries(data).map(([key, items]) => (
+          <ItemList key={key}>
+            <Text className="bold">{key}</Text>
+            {items.map((item) => (
+              <Text>{item}</Text>
+            ))}
+          </ItemList>
+      ))}
+      </List>
+    </Section>
+  );
+}
