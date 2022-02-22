f93f9ffdfa03acd4b3dbbc969b8e8533df6ed5a9
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-02-22 13:26:59 -0300

    add route for portfolio page

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import PresentationPage from './pages/PresentationPage/';
+import PortfolioPage from './pages/PortfolioPage/';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<PresentationPage />} />
+        <Route exact path='/portfolio' element={<PortfolioPage />} />
       </Routes>
     </BrowserRouter>
   );
