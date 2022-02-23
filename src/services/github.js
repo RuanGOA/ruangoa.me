991f69949be3c49500158b74f958206fd77213bb
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-02-23 19:39:43 -0300

    add github service

---
+++ src/services/github.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const GITHUB_API_URL = 'https://api.github.com';
+
+const APIGithub = axios.create({
+	baseURL: GITHUB_API_URL,
+	headers: {
+		'Content-Type': 'application/json'
+	},
+});
+
+export const github = {
+	async searchRepos(repos) {
+		const response = APIGithub.get('/users/ruangoa/repos?per_page=300')
+			.then(response => {
+				const repositories = response.data.filter((repo) => repos.includes(repo.name.toLowerCase()));
+				return repositories.map((repo) => {
+					return {
+						name: repo.name,
+						description: repo.description ? repo.description.toString('utf8') : '...',
+						mainLanguage: repo.language,
+						stars: repo.stargazers_count,
+						forks: repo.forks_count,
+						url: repo.html_url,
+					}
+				})
+			});
+		return response;
+	}
+};
