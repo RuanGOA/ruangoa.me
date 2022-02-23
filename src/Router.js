76d8cd855f2175c8ebff251dd601d625e26144e2
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-02-23 19:40:29 -0300

    add project page to router

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import PresentationPage from './pages/PresentationPage/';
 import PortfolioPage from './pages/PortfolioPage/';
+import ProjectsPage from './pages/ProjectsPage/';
 
 export default function Router() {
   return (
@@ -11,6 +12,7 @@ export default function Router() {
       <Routes>
         <Route exact path='/' element={<PresentationPage />} />
         <Route exact path='/portfolio' element={<PortfolioPage />} />
+        <Route exact path='/projects' element={<ProjectsPage />} />
       </Routes>
     </BrowserRouter>
   );
