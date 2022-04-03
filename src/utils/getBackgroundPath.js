bae25e1446638898b8cc1bc4e6ba43073f2bab14
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-04-03 11:35:21 -0300

    add background 6

--- src/utils/getBackgroundPath.js
+++ src/utils/getBackgroundPath.js
@@ -3,6 +3,7 @@ import Image2 from '../assets/backgrounds/image2.jpeg';
 import Image3 from '../assets/backgrounds/image3.jpeg';
 import Image4 from '../assets/backgrounds/image4.jpeg';
 import Image5 from '../assets/backgrounds/image5.jpeg';
+import Image6 from '../assets/backgrounds/image6.jpeg';
 
 export function getBackgroundPath() {
   const backgroundList = [
@@ -11,6 +12,7 @@ export function getBackgroundPath() {
     Image3,
     Image4,
     Image5,
+    Image6,
   ];
 
   return backgroundList[Math.floor((Math.random() * backgroundList.length))];
