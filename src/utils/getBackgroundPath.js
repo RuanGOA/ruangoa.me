d859d08e08d1e5b4a367f080de38bc37b01533cb
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-07-30 13:30:02 -0300

    improve background image load

--- src/utils/getBackgroundPath.js
+++ src/utils/getBackgroundPath.js
@@ -1,19 +1,7 @@
-import Image1 from '../assets/backgrounds/image1.jpeg';
-import Image2 from '../assets/backgrounds/image2.jpeg';
-import Image3 from '../assets/backgrounds/image3.jpeg';
-import Image4 from '../assets/backgrounds/image4.jpeg';
-import Image5 from '../assets/backgrounds/image5.jpeg';
-import Image6 from '../assets/backgrounds/image6.jpeg';
-
 export function getBackgroundPath() {
-  const backgroundList = [
-    Image1,
-    Image2,
-    Image3,
-    Image4,
-    Image5,
-    Image6,
-  ];
+  const backgroundQuantity = 6; //fs doesn't work in browser
+  const randomNumber = Math.floor((Math.random() * backgroundQuantity) + 1);
+  const image = require(`../assets/backgrounds/image${randomNumber}.jpeg`);
 
-  return backgroundList[Math.floor((Math.random() * backgroundList.length))];
+  return image
 }
