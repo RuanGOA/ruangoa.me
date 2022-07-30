d14e274e4a099ae3beb56797569b692c433b1602
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-07-30 13:29:39 -0300

    fix linter error

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -4,8 +4,6 @@ import { useNavigate } from 'react-router-dom'
 
 import { useConfig } from '../../contexts/config.context';
 
-import { removeAccents } from '../../utils/removeAccents';
-
 import {
   PostContainer,
   PostName,
