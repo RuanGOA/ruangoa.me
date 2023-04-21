8a00800908a84bff90351b0770520ae38cad3e7f
Ruan Gomes <ruangoa0@gmail.com>
2023-04-20 23:46:42 -0300

    fix list types in post

--- components/PostContent/index.js
+++ components/PostContent/index.js
@@ -7,7 +7,7 @@ export default function PostContent({ post }) {
   return (
     <div className="px-4 lg:px-[22%] max-w bg-background-dark">
       <div className={`px-[3%] py-4 lg:py-12 items-center gap-8 flex-col lg:flex-row lg:justify-center justify-center`}>
-        <article dangerouslySetInnerHTML={{ __html: htmlText }} className="!text-white text-justify prose-xl"/>;
+        <div dangerouslySetInnerHTML={{ __html: htmlText }} className="text-white prose-lg prose-li:list-disc prose-code:bg-white prose-code:py-[0.1em] prose-code:px-1 prose-code:rounded-md prose-code:text-black"/>;
       </div>
     </div>
   );
