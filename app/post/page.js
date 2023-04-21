fb7c2377f78dc5a993710a10cff1bcc54971d4aa
Ruan Gomes <ruangoa0@gmail.com>
2023-04-21 00:11:43 -0300

    refactoring post page

--- app/post/page.js
+++ app/post/page.js
@@ -1,42 +1,15 @@
-"use client"
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
-
-import { github } from "../../services/github";
-
-import Loading from "../../components/Loading";
 import PostContent from "../../components/PostContent";
 
-export default function Post() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [post, setPost] = useState({});
-  const [doSearch, setDoSearch] = useState(true);
-
-  const useSearch = useSearchParams();
-
-  const getPost = async (index) => {
-    const postReq = await github.getPost(index);
-    setPost(postReq);
-    setIsLoading(false);
+export async function generateMetadata({ params }) {
+  return {
+    title: `@ruangoa - Post`,
   }
+}
 
-  useEffect(() => {
-    if (doSearch && useSearch.get("index")) {
-      const postIndex = useSearch.get("index");
-
-      getPost(postIndex);
-
-      setDoSearch(false);
-    }
-  }, [useSearch, doSearch]);
-
+export default function Post() {
   return (
     <main className="max-w min-h-screen bg-background-dark">
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <PostContent post={post} />
-      )}
+      <PostContent />
     </main>
   )
 }
