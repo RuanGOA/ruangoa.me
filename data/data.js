7af9e9ee9a39675deb75a99eca20550ef9b3c444
Ruan Gomes <ruangoa0@gmail.com>
2023-04-24 18:16:19 -0300

    update certificate and honors urls

--- data/data.js
+++ data/data.js
@@ -1,10 +1,14 @@
+export const name = "Ruan Gomes";
+
+export const job = "Reliability Researcher";
+
 export const sectionAbout = {
   title: "Hi, I'm Ruan 👋",
   text: [
     "I’m finishing CS bachelor degree, but my first contact with technology was in 2014, when I started a technical computer course. Since then, I've been applying technology knowledge to projects, aiming to build interesting things.",
     "I participated in some interesting partnership projects in college, which mainly involved: web development; researches about infrastructure, software architecture and artificial intelligence; and improvement of infrastructure efficiency."
   ]
-}
+};
 
 export const sectionResume = {
   title: "Resume",
@@ -41,23 +45,23 @@ export const sectionResume = {
     text: [
       {
         title: "Silver Medal - Paraíba State Computer Olympiad [2022]",
-        url: "#",
+        url: "http://www.dsc.ufcg.edu.br/~opi/resultados/2022/resultado-avancado-senior-2022.pdf",
       },
       {
-        title: "Silver Medal - Paraíba State Computer Olympiad [2021]",
-        url: "#",
+        title: "Silver Medal - Paraíba State Computer Olympiad [2019]",
+        url: "http://www.dsc.ufcg.edu.br/~opi/resultados/2019/resultado-avancado-junior-2019.pdf",
       },
       {
         title: "Honorable Mention - Brazilian Astronomy Olympiad [2016]",
-        url: "#",
+        url: "https://drive.google.com/file/d/174YUPsHuYo-8eYLTXAf63rZnX04UXggT/view?usp=sharing",
       },
       {
         title: "Honorable Mention - Brazilian Public School Mathematics Olympiad [2016]",
-        url: "#",
+        url: "https://drive.google.com/file/d/195_9XVkCFIVmHqiaUtqyywob1jHBwsPg/view?usp=sharing",
       },
       {
         title: "Honorable Mention - Brazilian Astronomy Olympiad [2014]",
-        url: "#",
+        url: "https://drive.google.com/file/d/11GPkPepdHDYVzXfYoXsPyAf9rttfJLL0/view?usp=sharing",
       }
     ]
   },
@@ -66,23 +70,23 @@ export const sectionResume = {
     text: [
       {
         title: "AWS Cloud Practitioner Essentials [2023]",
-        url: "#",
+        url: "https://drive.google.com/file/d/1Npe1OSeM6Kmmi_okJZQRbHv1l-7yTEEK/view?usp=sharing",
       },
       {
         title: "RedHat System Administration I (RH124) [2022]",
-        url: "#",
+        url: "https://drive.google.com/file/d/12bG82AeWooshzf9RD8zq9MIu1PV_cEPy/view?usp=sharing",
       },
       {
         title: "OpenShift I: Containers & Kubernetes (DO180) [2021]",
-        url: "#",
+        url: "https://drive.google.com/file/d/1DNxvzbv6-_QWH065s8BxK32cgM088PFn/view?usp=sharing",
       },
       {
         title: "SCRUM Fundamentals [2021]",
-        url: "#",
+        url: "https://drive.google.com/file/d/17Ojmf-Ghb-Kf1jTqUAcLO32PVY9jVYNu/view?usp=sharing",
       },
     ]
   }
-}
+};
 
 export const sectionContact = {
   title: "Contact",
@@ -106,19 +110,45 @@ export const sectionContact = {
       url: "#",
     }
   ]
-}
+};
 
 export const pageSelect = [
-/*  {*/
-    /*title: "About",*/
-    /*url: "/about",*/
-/*  },*/
   {
     title: "Resume",
     url: "/",
   },
   {
+    title: "About",
+    url: "/about",
+  },
+  {
     title: "Posts",
     url: "/posts",
-  }
-]
+  },
+];
+
+export const sectionMe = {
+  title: "Me in X minutes",
+  text: `I’m a curious INTJ guy. I like to learn new things about technology, neuroscience, philosophy, and some other areas. My hobbies involve things like reading, coding, playing video games and chess, and exercises like gym, fights, biking, and running. And yes! I have many hobbies, and I intend to have more hahaha. Oh, and I like to travel to know new places.`
+};
+
+export const sectionNow = {
+  title: "Right now",
+  text: [
+    "I’m working on cost reductions in cloud infrastructure. I’m working on my final paper too, and in general words, it’s about how to reduce the total cache capacity.",
+    "I’m learning some frontend technologies, like server-side rendering with NextJS and CSS frameworks like TailwindCSS. And I’m reading about site reliability, looking for infrastructure improvements and technologies that help to keep services efficient and reliable.",
+    "I’m training 4~5x by week, which permits me to focus on what matters.",
+    "And lately, I’m trying to get back to chess and language studies."
+  ]
+};
+
+export const sectionFuture = {
+  title: "About the future...",
+  text: "I try to don’t think about things I don’t have control over, but my immediate goals are:",
+  points: [
+    "Finish the C.S. Bsc.",
+    "Train until I can run a half marathon",
+    "Improve my knowledge about technology",
+    "Create some posts about my life perceptions, things that I’m studying and things like that"
+  ],
+};
