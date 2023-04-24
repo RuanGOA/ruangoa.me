6987e69a488bff8c38c6ea7f09e1b091531a54b2
Ruan Gomes <ruangoa0@gmail.com>
2023-04-23 21:27:20 -0300

    fix post tag align

--- components/PostList/components/PostCard/index.js
+++ components/PostList/components/PostCard/index.js
@@ -9,7 +9,7 @@ export default function PostCard({ post }) {
         <div className="flex w-full flex-col gap-2">
           <div className="flex justify-between w-full">
             <h3 className="text-2xl text-highlight font-bold">{post.title}</h3>
-            <p className="text-black py-[0.05em] px-2 bg-white rounded-md">{post.label}</p>
+            <p className="text-black py-[0.05em] px-2 bg-white rounded-md flex items-center">{post.label}</p>
           </div>
           <p className="text-white">{post.createdAt}</p>
           <p className="text-white">{post.timeRead} minutes read</p>
