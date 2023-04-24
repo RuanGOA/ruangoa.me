dc78f60a7e761505e8b7b906fefc799a1d89f7b1
Ruan Gomes <ruangoa0@gmail.com>
2023-04-23 21:24:54 -0300

    fix post label

--- components/PostList/components/PostCard/index.js
+++ components/PostList/components/PostCard/index.js
@@ -9,9 +9,7 @@ export default function PostCard({ post }) {
         <div className="flex w-full flex-col gap-2">
           <div className="flex justify-between w-full">
             <h3 className="text-2xl text-highlight font-bold">{post.title}</h3>
-            <p className="text-black py-[0.05em] px-2 bg-white rounded-md cursor-pointer hover:scale-110 transition-scale duration-1000">
-              {post.label}
-            </p>
+            <p className="text-black py-[0.05em] px-2 bg-white rounded-md">{post.label}</p>
           </div>
           <p className="text-white">{post.createdAt}</p>
           <p className="text-white">{post.timeRead} minutes read</p>
