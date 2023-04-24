c8af9015288354205d013086187d60d414810593
Ruan Gomes <ruangoa0@gmail.com>
2023-04-23 23:08:46 -0300

    change name to bold in footer text

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <footer className="flex items-center max-w justify-center py-6 bg-background-dark">
-      <p className="text-highlight">Developed by Ruan Gomes | bit by bit 🚀</p>
+      <p className="text-highlight">Developed by <span className="font-bold">Ruan Gomes</span> | bit by bit 🚀</p>
     </footer>
   )
 }
