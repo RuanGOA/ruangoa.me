93bd2e01e9ca28fada86f270adc9d2b7ad405226
Ruan Gomes <ruan.alves@splab.ufcg.edu.br>
2025-04-19 20:57:23 -0300

    fix: improve routing

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const exp = {
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/blog',
+        source: '/blog',
+        destination: '/',
         permanent: true,
       },
     ];
