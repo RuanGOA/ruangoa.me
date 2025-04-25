d5df86f064f8570b7ed07589dae4b25ac40ceb0d
Ruan Gomes <ruangoa0@gmail.com>
2025-04-25 16:57:26 -0300

    feat: add target blank to anchor tag

--- src/components/LocalMarkdownRenderer.js
+++ src/components/LocalMarkdownRenderer.js
@@ -1,9 +1,19 @@
 import ReactMarkdown from 'react-markdown';
 
+function LinkRenderer({ children, href }) {
+  return (
+    <a href={href} target="_blank" rel="noreferrer">
+      {children}
+    </a>
+  );
+}
+
 export default function LocalMarkdownRenderer({ children }) {
   return (
     <div className="max-w-none w-full lg:w-2/3 p-4 lg:p-0 prose dark:prose-invert justify-center">
-      <ReactMarkdown>{children}</ReactMarkdown>
+      <ReactMarkdown components={{ a: LinkRenderer }}>
+        {children}
+      </ReactMarkdown>
     </div>
   );
 }
